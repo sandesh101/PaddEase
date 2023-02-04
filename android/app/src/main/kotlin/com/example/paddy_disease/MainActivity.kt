422a5067a6d24c8ef3b411e13d1c506921957a7d
Sandesh Rimal <rimal.sandesh11@gmail.com>
2023-02-04 09:55:22 +0545

    Initial

---
+++ android/app/src/main/kotlin/com/example/paddy_disease/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.paddy_disease
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
