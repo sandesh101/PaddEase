e5525c73b75d6f1d64e3683d13a9bd39b4e8a7a4
sandesh101 <rimal.sandesh11@gmail.com>
2023-05-02 23:13:35 +0545

    Created Weather API Model

--- lib/model/weather_model.dart
+++ lib/model/weather_model.dart
@@ -1,4 +1,65 @@
 class WeatherModel {
-  String? cityName;
+  List<Weather>? weather;
+  Main? main;
+  String? name;
+
+  WeatherModel({this.weather, this.main, this.name});
+
+  WeatherModel.fromJson(Map<String, dynamic> json) {
+    if (json['weather'] != null) {
+      weather = <Weather>[];
+      json['weather'].forEach((v) {
+        weather!.add(new Weather.fromJson(v));
+      });
+    }
+    main = json['main'] != null ? new Main.fromJson(json['main']) : null;
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.weather != null) {
+      data['weather'] = this.weather!.map((v) => v.toJson()).toList();
+    }
+    if (this.main != null) {
+      data['main'] = this.main!.toJson();
+    }
+    data['name'] = this.name;
+    return data;
+  }
+}
+
+class Weather {
+  String? description;
+  String? icon;
+
+  Weather({this.description, this.icon});
+
+  Weather.fromJson(Map<String, dynamic> json) {
+    description = json['description'];
+    icon = json['icon'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['description'] = this.description;
+    data['icon'] = this.icon;
+    return data;
+  }
+}
+
+class Main {
   double? temp;
+
+  Main({this.temp});
+
+  Main.fromJson(Map<String, dynamic> json) {
+    temp = json['temp'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['temp'] = this.temp;
+    return data;
+  }
 }
